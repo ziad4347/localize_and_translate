18359d51d6baf9dc57459926fac21070ca437ecb
[ziad4347] <71031166+ziad4347@users.noreply.github.com>
2025-01-30 17:19:59 +0200

    update

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/msayed/Documents/projects/palestine/localize_and_translate"
+export "FLUTTER_ROOT=C:\Users\Ziad\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutter\flutter-p\upated_packages\localize_and_translate"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=4.1.3"
-export "FLUTTER_BUILD_NUMBER=4.1.3"
+export "FLUTTER_BUILD_NAME=5.1.2"
+export "FLUTTER_BUILD_NUMBER=5.1.2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
